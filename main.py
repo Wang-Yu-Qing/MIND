4e0d96facb790b2227c391a0423efc47ed8c8058
王宇清 <413022472@qq.com>
2022-06-26 10:44:33 +0800

    build samples

---
+++ main.py
@@ -0,0 +1,23 @@
+import itertools
+from utils import *
+
+
+if __name__ == '__main__':
+    args = parse_args()
+
+    valid_users, valid_books = get_valid_user_and_book_set(args.data_frac)
+    all_user_rates = parse_rating_data(valid_users, valid_books)
+    book_cates = read_book_meta(valid_books)
+    valid_cates = set(itertools.chain.from_iterable(list(book_cates.values())))
+
+    user_encoder, user_decoder = encode_users(valid_users)
+    book_encoder, book_decoder = encode_books(valid_books)
+    cate_encoder, cate_decoder = encode_cates(valid_cates)
+
+    all_train_rates, all_test_rates = split_train_and_test_rates(all_user_rates, args.train_frac)
+
+    train_samples = build_samples(all_train_rates, all_user_rates)
+    test_samples = build_samples(all_test_rates, all_user_rates, valid_books, args)
+
+    pkl_save("data/train_samples.pkl", train_samples)
+    pkl_save("data/test_samples.pkl", test_samples)
---
+++ utils.py
@@ -0,0 +1,166 @@
+import json
+import pickle
+import random
+import argparse
+import numpy as np
+import pandas as pd
+
+
+def parse_args():
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('--data_frac', type=float, default=0.3)
+    argparser.add_argument('--min_user_freq', type=int, default=10)
+    argparser.add_argument('--min_book_freq', type=int, default=10)
+    argparser.add_argument('--max_user_freq', type=int, default=200)
+    argparser.add_argument('--train_frac', type=int, default=0.95)
+    argparser.add_argument('--his_len', type=int, default=100)
+    argparser.add_argument('--n_neg', type=int, default=10)
+
+    args = argparser.parse_args()
+
+    return args
+
+
+def get_valid_user_and_book_set(data_frac):
+    user_freq, book_freq = {}, {}
+    with open('data/ratings_Books.csv', 'r') as f:
+        for line in f.readlines():
+            if random.random() < data_frac:
+                # user, item, rating, timestamp
+                splitted = line.split(",")
+                try:
+                    user_freq[splitted[0]] += 1
+                except KeyError:
+                    user_freq[splitted[0]] = 1
+                try:
+                    book_freq[splitted[1]] += 1
+                except KeyError:
+                    book_freq[splitted[1]] = 1
+
+    valid_users = set([user_id for user_id, cnt in user_freq.items() if cnt >= args.min_user_freq and cnt <= args.max_user_freq])
+    valid_books = set([book_id for book_id, cnt in book_freq.items() if cnt >= args.min_book_freq])
+
+    return valid_users, valid_books
+    
+
+def parse_rating_data(valid_users, valid_books):
+    user_rates = {}
+    with open('data/ratings_Books.csv', 'r') as f:
+        for line in f.readlines():
+            line = line[:-1]
+            splitted = line.split(",")
+            user_id, book_id = splitted[0], splitted[1]
+            if user_id in valid_users and book_id in valid_books:
+                rate = [user_id, book_id, int(splitted[-1])]
+                try:
+                    user_rates[user_id].append(rate)
+                except KeyError:
+                    user_rates[user_id] = [rate]
+    
+    # convert user ratings to pd.DataFrame for efficient history quering
+    for user_id, rates in user_rates.items():
+        user_rates[user_id] = pd.DataFrame(rates, columns=['user_id', 'book_id', 'timestamp']).sort_values(by='timestamp', ignore_index=True)
+
+    return user_rates
+
+
+def read_book_meta(valid_books):
+    book_cates = {}
+    with open("data/meta_Books.json", "r") as f:
+        for line in f.readlines():
+            meta = json.loads(line[:-1])
+            if meta['asin'] in valid_books:
+                book_cates[meta['asin']] = meta['category']
+
+    return book_cates
+
+
+def encode_users(valid_users):
+    encoder, decoder = {}, []
+    encode_id = 0
+    for user_id in valid_users:
+        if user_id not in encoder:
+            encoder[user_id] = encode_id
+            decoder.append(user_id)
+            encode_id += 1
+
+    return encoder, decoder
+
+
+def encode_books(valid_books):
+    encoder, decoder = {'<pad>': 0}, ['<pad>']
+    encode_id = 1
+    for book_id in valid_books:
+        if book_id not in encoder:
+            encoder[book_id] = encode_id
+            decoder.append(book_id)
+            encode_id += 1
+    
+    return encoder, decoder
+
+
+def encode_cates(book_cates):
+    encoder, decoder = {'<pad>': 0}, ['<pad>']
+    encode_id = 1
+    for cate in book_cates:
+        if cate not in encoder:
+            encoder[cate] = encode_id
+            decoder.append(cate)
+            encode_id += 1
+    
+    return encoder, decoder
+
+
+def split_train_and_test_rates(user_rates, train_frac):
+    all_train_ratings, all_test_ratings = [], []
+    for user_id, ratings in user_rates.items():
+        train_rates = ratings.sample(frac=train_frac, replace=False)
+        test_rates = ratings.drop(train_rates.index)
+
+        for rate in train_rates.values: all_train_ratings.append(rate)
+        for rate in test_rates.values: all_test_ratings.append(rate)
+    
+    return all_train_ratings, all_test_ratings
+
+
+def pad_or_cut(seq, length):
+    if len(seq) > length:
+        # cut the front
+        # len(seq) - 1 - x + 1 = length --> x = len(seq) - length
+        return seq[len(seq) - length:]
+    else:
+        return np.concatenate((seq, np.array([0] * (length - len(seq)))))
+
+
+def query_history_books(user_id, timestamp, user_rates, his_len):
+    bool_idx = user_rates['timestamp'] < timestamp
+    history_books = user_rates['timestamp'].loc[bool_idx, ['book_id']].values
+    if len(history_books) != his_len:
+        history_books = pad_or_cut(history_books, his_len)
+
+    return history_books
+
+
+def build_samples(rates, all_user_rates, valid_books, args):
+    samples = []
+    candidates = list(valid_books)
+    for rate in rates:
+        his = query_history_books(rate[0], rate[2], all_user_rates[rate[0]], args.his_len)
+        tar = rate[1]
+        samples.append({'his': his, 'tar': tar, 'label': 1})
+        # negative sampling
+        neg_tar = random.choices(candidates, k = args.n_neg)
+        for tar in neg_tar:
+            samples.append({'his': his, 'tar': tar, 'label': 0})
+    
+    return samples
+
+
+def pkl_save(filepath, obj):
+    with open(filepath, "wb") as f:
+        f.write(pickle.dumps(obj))
+
+
+def pkl_read(filepath, obj):
+    with open(filepath, "rb") as f:
+        return pickle.loads(f.read())
